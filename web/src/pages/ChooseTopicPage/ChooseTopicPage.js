21156eccf2e324ad8686b87cd22b15c9ff9ca976
Eric Lake <ericallenlake@gmail.com>
2022-10-07 15:07:14 -0600

    small improvements

--- web/src/pages/ChooseTopicPage/ChooseTopicPage.js
+++ web/src/pages/ChooseTopicPage/ChooseTopicPage.js
@@ -38,7 +38,7 @@ const ChooseTopicPage = () => {
       <ul className="max-w-full overflow-hidden">
         <li className="custom-max flex justify-center ">
           <div className="card  max-w-11/12 relative mb-5 flex flex-col items-center justify-center rounded-lg   lg:m-2 lg:block  xl:w-11/12">
-            <div className="p2 z-10 mx-auto flex  w-min rounded-lg bg-white shadow items-center gap-4">
+            <div className="p2 z-10 mx-auto flex  w-min items-center gap-4 rounded-lg shadow">
               <p className="text-2xl font-extrabold ">Health</p>
               <div className="flex w-11/12 justify-between gap-1">
                 <i className="fa-duotone fa-user p-1 "></i> 24
@@ -57,23 +57,23 @@ const ChooseTopicPage = () => {
                 <Response response={response3} />
               </article>
             </div>
-            <button className="p2 z-10 m-4 mx-auto  flex w-min rounded-lg bg-white shadow">
+            <button className="p2 z-10 m-4 mx-auto  flex w-min rounded-lg shadow">
               <p className="font-bold">Subscribe</p>
             </button>
           </div>
         </li>
         <li className="custom-max flex justify-center ">
-          <div className="card shadow-p relative mb-5 flex flex-col items-center justify-center rounded-lg bg-emerald-50 lg:m-2 lg:block xl:w-11/12">
-            <div className="p2 top bg z-10 flex w-min items-center gap-2 rounded-lg shadow lg:absolute">
-              <p className="text-2xl font-extrabold">Health</p>
-              <div className="flex w-11/12 justify-between">
-                <i className="fa-duotone fa-user p-1 text-black"></i> 24
+          <div className="card  max-w-11/12 relative mb-5 flex flex-col items-center justify-center rounded-lg   lg:m-2 lg:block  xl:w-11/12">
+            <div className="p2 z-10 mx-auto flex  w-min items-center gap-4 rounded-lg shadow">
+              <p className="text-2xl font-extrabold ">Health</p>
+              <div className="flex w-11/12 justify-between gap-1">
+                <i className="fa-duotone fa-user p-1 "></i> 24
               </div>
-              <div className="flex w-11/12 justify-between">
+              <div className="flex w-11/12 justify-between gap-1">
                 <i className="fa-duotone fa-coin p-1"></i> 4200
               </div>
             </div>
-            <h1 className="mb-3 mt-5 text-center text-2xl font-bold">
+            <h1 className="m-x-auto mb-3 mt-5 p-2 text-center text-2xl  font-bold">
               Jeans: High Rise or Low?{' '}
             </h1>
             <div className="container flex justify-center">
@@ -83,11 +83,12 @@ const ChooseTopicPage = () => {
                 <Response response={response3} />
               </article>
             </div>
-            <button className="p2 bg bottom-3 w-min  rounded-lg shadow 2xl:absolute">
+            <button className="p2 z-10 m-4 mx-auto  flex w-min rounded-lg shadow">
               <p className="font-bold">Subscribe</p>
             </button>
           </div>
         </li>
+
       </ul>
     </>
   )
