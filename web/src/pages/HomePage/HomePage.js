1d55b6f5e4f79ff7a05de30682b8fcfc5b709746
Eric Lake <ericallenlake@gmail.com>
2022-10-10 00:08:23 -0600

    Favicon + metadata

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -5,7 +5,7 @@ import GetResponseCell from 'src/components/ResponseCardsCell'
 const HomePage = () => {
   return (
     <>
-      <MetaTags title="Home" description="Home page" />
+      <MetaTags title="Home" description="Talking Cents Home page" />
       <main className="flex items-center justify-center flex-wrap">
         <GetResponseCell />
       </main>
