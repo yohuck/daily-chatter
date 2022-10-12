dc8e36ed04cbc539c54ff382da990fbc8388cd6e
Eric Lake <ericallenlake@gmail.com>
2022-10-12 17:54:22 -0600

    Footer loader on home

--- web/src/components/SubmitYourResponsesCell/LoopResponses.js
+++ web/src/components/SubmitYourResponsesCell/LoopResponses.js
@@ -2,7 +2,7 @@ import PromptInput from '../PromptInput/PromptInput'
 
 const LoopResponses = ({ filteredPrompts }) => {
   return filteredPrompts.map((prompt) => (
-    <div key={prompt.id} className="z-20 relative flex justify-center">
+    <div key={prompt.id} className="relative z-20 flex justify-center">
       <PromptInput prompt={prompt} />
     </div>
   ))
