aeeb5acdd7146f5767dc435555bd0906eabad9a3
jdog <thuz3rfr13ndly@gmail.com>
2022-10-12 19:48:13 -0600

    itworks

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -7,10 +7,8 @@ import services from 'src/services/**/*.{js,ts}'
 import { getCurrentUser } from 'src/lib/auth'
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
-
 export const handler = createGraphQLHandler({
   getCurrentUser,
-
   loggerConfig: { logger, options: {} },
   directives,
   sdls,
